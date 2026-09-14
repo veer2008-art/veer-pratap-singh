64e7db311149f97ff9967a42e2a24401129192c4
veer2008-art <veer.31804@stu.upes.ac.in>
2026-09-14 10:38:59 +0530

    Create day11.c

---
+++ day11.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0)
+    {
+        printf("%d is Even", num);
+    }
+    else
+    {
+        printf("%d is Odd", num);
+    }
+
+    return 0;
+}
