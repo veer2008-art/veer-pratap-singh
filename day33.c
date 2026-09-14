deafdaeae4e8e775887870ccd47e06ffd03495f0
veer2008-art <veer.31804@stu.upes.ac.in>
2026-09-14 11:19:06 +0530

    Create day33.c

---
+++ day33.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, original, digit, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    original = n;
+
+    while(n != 0)
+    {
+        digit = n % 10;
+        sum = sum + digit * digit * digit;
+        n = n / 10;
+    }
+
+    if(sum == original)
+    {
+        printf("%d is an Armstrong number", original);
+    }
+    else
+    {
+        printf("%d is not an Armstrong number", original);
+    }
+
+    return 0;
+}
