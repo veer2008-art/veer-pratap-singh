18e411ab7a49d2b17b01cf56b8c4702e14066648
veer2008-art <veer.31804@stu.upes.ac.in>
2026-09-14 11:22:26 +0530

    Add prime number checker in day34.c

---
+++ day34.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, flag = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if(n <= 1)
+    {
+        flag = 1;
+    }
+    else
+    {
+        for(i = 2; i < n; i++)
+        {
+            if(n % i == 0)
+            {
+                flag = 1;
+                break;
+            }
+        }
+    }
+
+    if(flag == 0)
+    {
+        printf("%d is a Prime number", n);
+    }
+    else
+    {
+        printf("%d is not a Prime number", n);
+    }
+
+    return 0;
+}
